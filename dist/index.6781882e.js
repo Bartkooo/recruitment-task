631c7ed4858c61465f28b4d096c0b9f1ce5574e0
Bartkooo <bartosz.plochocki@gmail.com>
2022-07-18 16:03:05 +0200

    scss cleanup

--- dist/index.6781882e.js
+++ dist/index.6781882e.js
@@ -1,25 +1,25 @@
-const left_arrow = document.getElementById('arrow-left')
-const right_arrow = document.getElementById('arrow-right')
-left_arrow.addEventListener('click', changeIMGleft)
-right_arrow.addEventListener('click', changeIMGright)
+const left_arrow = document.getElementById("arrow-left");
+const right_arrow = document.getElementById("arrow-right");
+left_arrow.addEventListener("click", changeIMGleft);
+right_arrow.addEventListener("click", changeIMGright);
 const colors = [
-  '#7D8587',
-  '#41636c',
-  '#4fab3f',
-  '#825338',
-  '#533882',
-  '#828138',
-]
-let x = 0
+    "#7D8587",
+    "#41636c",
+    "#4fab3f",
+    "#825338",
+    "#533882",
+    "#828138", 
+];
+let x = 0;
 function changeIMGleft() {
-  x--
-  if (x < 0) x = colors.length - 1
-  document.getElementById('bg-image').style.background = colors[x]
+    x--;
+    if (x < 0) x = colors.length - 1;
+    document.getElementById("bg-image").style.background = colors[x];
 }
 function changeIMGright() {
-  x++
-  if (x == colors.length) x = 0
-  document.getElementById('bg-image').style.background = colors[x]
+    x++;
+    if (x == colors.length) x = 0;
+    document.getElementById("bg-image").style.background = colors[x];
 }
 
 //# sourceMappingURL=index.6781882e.js.map
